dbebd7d8bbc95abd7cebb4bf5312043f02fe0748
aissyyy <91513350+aissyyy@users.noreply.github.com>
2021-10-22 11:54:55 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main{
+public static void main(String[] args) {
+    Heroes hero = new Heroes();
+    hero.setProfile("Aisyah", 21, 160);
+    hero.profile();
+
+    hero.jalan();
+    hero.jalan();
+    hero.jalan();
+    hero.lari();
+    hero.makan();
+    hero.makan();
+    hero.makan();
+    hero.makan();
+    hero.makan();
+    hero.minum();
+    hero.lompat();
+    hero.lompat();
+    hero.lompat();
+    hero.lompat();
+    hero.lompat();
+    hero.duduk();
+    hero.duduk();
+    hero.duduk();
+
+    // challenge grup 3
+
+    // tampilkan status terkini...
+    hero.profile();
+    
+  }
+}
+
+    
